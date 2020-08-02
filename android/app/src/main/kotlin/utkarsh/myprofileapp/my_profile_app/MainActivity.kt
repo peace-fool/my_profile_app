91c07c72d256ef2698a6aba71b609f40db837f71
utkc137-codeverse <utkarsh.learn2020@gmail.com>
2020-08-02 09:37:37 +0530

    #

---
+++ android/app/src/main/kotlin/utkarsh/myprofileapp/my_profile_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package utkarsh.myprofileapp.my_profile_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                backgroundImage: AssetImage('images/profile.jpg'),
+                radius: 50,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
